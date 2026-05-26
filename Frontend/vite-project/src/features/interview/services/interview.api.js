9f4cf83ca501f7092a7a31160455ae63928f4f54
Ayushmaan Singh <ayushmaansingh2212@gmail.com>
2026-05-26 18:09:11 +0530

    fix: replace all localhost:3000 with render url

--- Frontend/vite-project/src/features/interview/services/interview.api.js
+++ Frontend/vite-project/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ai-interview-prep-5rvb.onrender.com",
   withCredentials: true,
 });
 
